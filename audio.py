6b64bbf1fcf88f2cd9e94487706187edd736f1b5
Alex Müller <alexarnimueller@protonmail.com>
2022-10-22 21:02:40 +0200

    :tada: initial commit

---
+++ audio.py
@@ -0,0 +1,27 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import sys
+import pygame
+import RPi.GPIO as GPIO
+
+# define GPIO pins
+b_play = 1  # play button (green)
+b_next = 2  # next button (black)
+b_prev = 3  # previous button (black)
+l_gre = 4   # green LED (play)
+l_red = 5   # red LED (pause)
+
+# setup GPIO pins to IN or OUT mode with pulldown resistor
+GPIO.setmode(GPIO.BOARD)
+GPIO.setup(b_play, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+GPIO.setup(b_next, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+GPIO.setup(b_prev, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+GPIO.setup(l_gre, GPIO.OUT, pull_up_down=GPIO.PUD_DOWN)
+# start red LED in HIGH state
+GPIO.setup(l_red, GPIO.OUT, pull_up_down=GPIO.PUD_UP)
+
+# code goes here
+
+GPIO.cleanup()
+sys.exit()
