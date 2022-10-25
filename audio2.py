71d41b380056eae6bc697a815ad30f193c4dcf85
Alex Müller <alexarnimueller@protonmail.com>
2022-10-25 19:28:15 +0200

    :loudspeaker: audio files and mixer added

--- audio2.py
+++ audio2.py
@@ -1,6 +1,10 @@
 #! /usr/bin/env python
 # -*- coding: utf-8 -*-
 
+import os
+from time import sleep
+from pygame import mixer
+from collections import deque
 from gpiozero import Button, LED
 from signal import pause 
 
@@ -10,20 +14,36 @@ b_next = Button(20, pull_down=False)  # next button (black)
 b_prev = Button(12, pull_down=False)  # previous button (black)
 l_gre = LED(26)   # green LED (play)
 l_red = LED(13)   # red LED (pause)
-
 l_red.on()
 
-def next(): 
-    print("NEXT") 
+audiofiles = deque([f"./audiofiles/{f}" for f in os.listdir("./audiofiles")])
 
-def prev(): 
+mixer.init()
+mixer.Sound(audiofiles[0])
+
+def next():
+    mixer.stop()
+    audiofiles.rotate(1)
+    sleep(0.25)
+    mixer.Sound(audiofiles[0])
+    print("NEXT")
+
+def prev():
+    mixer.stop()
+    audiofiles.rotate(-1)
+    sleep(0.25)
+    mixer.Sound(audiofiles[0])
     print("PREVIOUS") 
 
 def play():
-    global l_gre, l_red
     l_red.off()
     l_gre.on()
-    print("PLAY")
+    if mixer.get_busy():
+        mixer.pause()
+        print("PAUSE")
+    else:
+        mixer.play()
+        print("PLAY")
 
 b_play.when_pressed = play 
 b_next.when_pressed = next 
